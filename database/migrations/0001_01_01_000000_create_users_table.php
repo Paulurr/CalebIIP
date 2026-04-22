0e697567c4b77dd1860f7b245a8f484fa9c5018f
Paulurr <ferpaurr2017@gmail.com>
2026-04-22 11:46:24 -0600

    v 0.0.1

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,17 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('rol_id');
             $table->string('name',100);
             $table->string('email',100)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password',255);
+            $table->string('password',190);
             $table->rememberToken();
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
+            $table->string('email',100)->primary();
+            $table->string('token',100);
             $table->timestamp('created_at')->nullable();
         });
 
